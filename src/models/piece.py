578d53cfda0d74fbf738eba9d1c11acb6c463eb3
Patrick MacMurchy <pmm4654@gmail.com>
2023-04-02 17:29:38 -0600

    Fix a weird issue with using a constant as an instance variable - it maintained a reference to the array so it would change every time you initialized another class of the same team name

--- src/models/piece.py
+++ src/models/piece.py
@@ -1,11 +1,12 @@
+import pdb
 class piece:
     def __init__(self, starting_point, valid_moveset, team_name = 'black'):
         self.starting_point = starting_point
         # TODO: convert valid_moveset into simple namespace
         # so we can build a SimpleNamespace (dot notation dictionary, basically)
         # or a normal dictionary of the valid moveset - it will be easier to work with
-        self.valid_moveset = valid_moveset 
         self.team_name = team_name
+        self.valid_moveset = self.convert_moveset_based_on_team(valid_moveset)
         self.turn_number = 0
         print(__class__.__name__)
 
@@ -16,3 +17,16 @@ class piece:
     
     def end_turn(self):
         self.turn_number += 1
+    
+    # convert the moveset based on team/starting position
+    # example: "up" for the black team is [-1, 0] - but if you're the white team
+    # "up" would be [1, 0] - so we just multiply the "y-axis" move move by -1
+    def convert_moveset_based_on_team(self, moveset):
+        if self.team_name == 'white':
+            converted_moveset = []
+            for move in moveset:
+                move[0] = move[0] * -1
+                converted_moveset.append(move)
+            return converted_moveset
+        else:
+            return self.valid_moveset
--- src/tests/models/test_pawn.py
+++ src/tests/models/test_pawn.py
@@ -1,7 +1,10 @@
-from unittest import TestCase
+import pytest
+import pdb
 from src.models.pawn import pawn
 from src.models.constants import MOVES
 
+
+# @pytest.mark.black_team
 def test_has_correct_valid_moveset():
     assert pawn(starting_point='fuck you').valid_moveset == [[-1, 0], [-2,0]]
 
@@ -9,3 +12,16 @@ def test_has_correct_valid_moveset_at_turn_2():
     my_pawn = pawn(starting_point='fuck you')
     my_pawn.end_turn()
     assert my_pawn.valid_moveset == [[-1, 0]]
+
+
+# @pytest.mark.white_team
+def test_has_correct_valid_moveset_white():
+    my_pawn = pawn(team_name='white', starting_point='fuck you')
+    assert my_pawn.valid_moveset == [[1, 0], [2, 0]]
+
+
+def test_has_correct_valid_moveset_at_turn_2_white():
+    my_pawn = pawn(team_name='white', starting_point='fuck you')
+    my_pawn.end_turn()
+    assert my_pawn.turn_number == 1
+    assert my_pawn.valid_moveset == [[1, 0]]
