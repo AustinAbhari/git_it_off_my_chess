474cdf533c6fad634dbfc66f33f92e8abdeb9e3e
Patrick MacMurchy <pmm4654@gmail.com>
2023-04-09 20:11:05 -0600

    Update king again - gd copy past

--- src/models/pieces/king.py
+++ src/models/pieces/king.py
@@ -1,12 +1,13 @@
 from .piece import piece
+from src.helpers.constants import MOVES
 
 
 class king(piece):
     def __init__(self, starting_point, team_name='black'):
         self.starting_point = starting_point
         # special handling for 2, 0 as ONLY valid for the first move
-        self.valid_moveset = [[1, 0], [0, 1], [-1, 0],
-                              [0, -1], [1, 1], [-1, -1], [-1, 1], [1, -1]]
+        self.valid_moveset = [MOVES.up, MOVES.down, MOVES.left, MOVES.right,
+                              MOVES.up_right, MOVES.up_left, MOVES.down_left, MOVES.down_right]
         self.team_name = team_name
         super(king, self).__init__(starting_point=starting_point,
                                    valid_moveset=self.valid_moveset, team_name=team_name)
