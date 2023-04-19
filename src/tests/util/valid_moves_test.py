263ca8ef7026b2d82195459fdfcd25eb51a5988a
AustinAbhari <aabmxer@gmail.com>
2023-04-18 20:44:22 -0600

    test for valid moves

--- src/tests/util/valid_moves_test.py
+++ src/tests/util/valid_moves_test.py
@@ -1,21 +1,32 @@
 import pytest
 from src.util.valid_moves import valid_moves
-import numpy as np
 from src.helpers.constants import MOVES
 
 
 def test_valid_moves_single_move():
-    move_set = [[1, 0], [0, 1], [-1, 0]]
+    move_set = [[MOVES.right]]
     current_position = [2, 2]
-    print(valid_moves(move_set, current_position))
+    valid_move_set = valid_moves(move_set, current_position)
+    expected_output = [[2, 3]]
+
+    assert len(valid_move_set) == 1
+    assert (valid_move_set[0] == expected_output[0]).all()
 
 
-# def test_move_set():
-#     moves = [
-#         [[row * x for row in MOVES.up_right] for x in range(1, 7)],
-#         [[row * x for row in MOVES.up_left] for x in range(1, 7)],
-#         [[row * x for row in MOVES.down_right] for x in range(1, 7)],
-#         [[row * x for row in MOVES.down_left] for x in range(1, 7)],
-#     ]
+def test_valid_moves_in_four_directions():
+    move_set = [[MOVES.right], [MOVES.left], [MOVES.up], [MOVES.down]]
+    current_position = [4, 4]
+    valid_move_set = valid_moves(move_set, current_position)
+    expected_output = [[4, 5], [4, 3], [3, 4], [5, 4]]
 
-#     print(valid_moves(moves, [4, 4]))
+    assert len(valid_move_set) == 4
+    for x in range(len(valid_move_set)):
+        assert (valid_move_set[x] == expected_output[x]).all()
+
+
+def test_valid_moves_boundaries():
+    move_set = [[MOVES.up], [MOVES.left]]
+    current_position = [0, 0]
+    valid_move_set = valid_moves(move_set, current_position)
+
+    assert not valid_move_set
--- src/util/valid_moves.py
+++ src/util/valid_moves.py
@@ -1,15 +1,18 @@
 import numpy as np
+import warnings
 
 
 def valid_moves(move_set, current_position):
-    boundary_floor = -1
-    bounday_ceil = 8
+    if type(move_set) != list and type(move_set) != current_position:
+        warnings.warn('bruh, move_set and current_position must be list')
+    boundary_floor = 0
+    bounday_ceil = 7
     moves = []
 
     for set in move_set:
         for move in set:
             m = np.add(current_position, move)
-            if any(i > 7 for i in m) or any(i < 0 for i in m):
+            if any(i > bounday_ceil for i in m) or any(i < boundary_floor for i in m):
                 break
             moves.append(m)
 
