848d965f0c182949bf4fc850fd15b7d2be20c2c8
AustinAbhari <aabmxer@gmail.com>
2023-04-28 01:13:24 -0600

    fun colors

--- client/src/components/square.js
+++ client/src/components/square.js
@@ -3,7 +3,7 @@ import { h } from 'preact';
 const Square = ({ color, piece, activeSquare, id, setActiveSquare, validSquares }) => (
     <div
         onClick={() => setActiveSquare(id)}
-        style={`background: ${validSquares.includes(id) ? 'yellow'
+        style={`background: ${validSquares.includes(id) ? '#9902f7'
             : id == activeSquare ? 'hotpink' : color};  
                 height: 80px;
                 width: 80px; 
